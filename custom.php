7424beed645d5dac530cd1026fa4960fe3db517d
jlopes1283 <39833151+jlopes1283@users.noreply.github.com>
2018-06-01 12:08:54 -0400

    Update custom.php

--- custom.php
+++ custom.php
@@ -11,6 +11,48 @@
 */
 /*--------------------------------------------------------------------------------------
 *
+* Gets Attachment ID from URL
+*
+*--------------------------------------------------------------------------------------*/
+
+function get_attachment_id( $url ) {
+	$home = home_url();
+	if(strpos($url, $home) !== false) {
+		$attachment_id = 0;
+		$dir = wp_upload_dir();
+		if ( false !== strpos( $url, $dir['baseurl'] . '/' ) ) {
+			$file = basename( $url );
+			$query_args = array(
+				'post_type'   => 'attachment',
+				'post_status' => 'inherit',
+				'fields'      => 'ids',
+				'meta_query'  => array(
+					array(
+						'value'   => $file,
+						'compare' => 'LIKE',
+						'key'     => '_wp_attachment_metadata',
+					),
+				)
+			);
+			$query = new WP_Query( $query_args );
+			if ( $query->have_posts() ) {
+				foreach ( $query->posts as $post_id ) {
+					$meta = wp_get_attachment_metadata( $post_id );
+					$original_file       = basename( $meta['file'] );
+					$cropped_image_files = wp_list_pluck( $meta['sizes'], 'file' );
+					if ( $original_file === $file || in_array( $file, $cropped_image_files ) ) {
+						$attachment_id = $post_id;
+						break;
+					}
+				}
+			}
+		}
+		return $attachment_id;
+	}
+}
+
+/*--------------------------------------------------------------------------------------
+*
 * Adds alt tags to images of the post
 *
 *--------------------------------------------------------------------------------------*/
